a56d70e4e103961d29fc7f0ca5b4282cd1fc43c9
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2026-03-10 11:56:29 -0300

    Updated Sobre.tsx

--- client/pages/Sobre.tsx
+++ client/pages/Sobre.tsx
@@ -238,10 +238,10 @@ export default function Sobre() {
       <section className="mx-6 mb-20">
         <div className="grid grid-cols-4 gap-6">
           <div className="bg-gray-200 rounded-3xl aspect-video overflow-hidden">
-            <img src="image1.mp4" alt="Gallery 1" className="w-full h-full object-cover" />
+            <img src="image1.png" alt="Gallery 1" className="w-full h-full object-cover" />
           </div>
           <div className="bg-gray-200 rounded-3xl aspect-video overflow-hidden">
-            <img src="/image2.mp4" alt="Gallery 2" className="w-full h-full object-cover" />
+            <img src="/image2.png" alt="Gallery 2" className="w-full h-full object-cover" />
           </div>
           <div className="bg-gray-200 rounded-3xl aspect-video overflow-hidden">
             <img src="image3.png" alt="Gallery 3" className="w-full h-full object-cover" />
